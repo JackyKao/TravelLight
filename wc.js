5b55ba78622ac5d82af898c1e8fd62a4b0f20a83
JackyKao <gaogenius2002@gmail.com>
2016-12-20 22:10:01 +0800

    Add files via upload

---
+++ wc.js
@@ -0,0 +1,48 @@
+function DifFood()
+{
+d3.csv("https://jackykao.github.io/TravelLight/DifFood.csv", 
+  function(data)
+  {
+    d3.wordcloud()
+      .size([500, 300])
+      .fill(d3.scale.ordinal().range(["#884400", "#448800", "#888800", "#444400"]))
+      .words(data)
+      .start();
+  });
+}
+function JapanFood()
+{
+d3.csv("https://jackykao.github.io/TravelLight/JapanFood.csv", 
+  function(data)
+  {
+    d3.wordcloud()
+      .size([500, 300])
+      .fill(d3.scale.ordinal().range(["#884400", "#448800", "#888800", "#444400"]))
+      .words(data)
+      .start();
+  });
+}
+function otherFood()
+{
+d3.csv("https://jackykao.github.io/TravelLight/otherFood.csv", 
+  function(data)
+  {
+    d3.wordcloud()
+      .size([500, 300])
+      .fill(d3.scale.ordinal().range(["#884400", "#448800", "#888800", "#444400"]))
+      .words(data)
+      .start();
+  });
+}
+function VegFood()
+{
+d3.csv("https://jackykao.github.io/TravelLight/VegFood.csv", 
+  function(data)
+  {
+    d3.wordcloud()
+      .size([500, 300])
+      .fill(d3.scale.ordinal().range(["#884400", "#448800", "#888800", "#444400"]))
+      .words(data)
+      .start();
+  });
+}
