fb5ffc6fbd47e72f550f9932629768631d226a13
JackyKao <gaogenius2002@gmail.com>
2016-12-26 18:18:31 +0800

    Update wc.js

--- wc.js
+++ wc.js
@@ -50,3 +50,16 @@ function VegFood()
       .start();
   });
 }
+function sweetFood()
+{
+  document.getElementById('wordcloud').innerHTML='';
+  d3.csv("https://jackykao.github.io/TravelLight/sweetFood.csv", 
+  function(data)
+  {
+    d3.wordcloud()
+      .size([500, 300])
+      .fill(d3.scale.ordinal().range(["#884400", "#448800", "#888800", "#444400"]))
+      .words(data)
+      .start();
+  });
+}
